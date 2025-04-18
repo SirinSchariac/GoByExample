820b4f41663a39f4e3f467af616c33b2fce3e2e8
SirinSchariac <bradyeverly057@gmail.com>
2025-04-18 14:11:29 +0800

    Upload Enums

---
+++ EnumsDemo.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "Idle",
+	StateConnected: "Connected",
+	StateError:     "Error",
+	StateRetrying:  "Retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+}
+
+func main() {
+	newState := transition(StateIdle)
+	fmt.Println(newState)
+
+	newState2 := transition(newState)
+	fmt.Println(newState2)
+}
